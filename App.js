7ce4e61336ff14651c6b26c1f8b96e73edd03d9e
milan-2604 <milan2003oli@gmail.com>
2025-11-22 19:28:16 +0530

    day 2 namaste-react ep2 done

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-/*
-<div id="parent">
-    div id="child">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-</div>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-
-*/
 const parent = React.createElement('div', {id: 'parent'},
     React.createElement('div',{id: 'child'},
-        [React.createElement('h1', {}, "I am h1 tag"),
-            React.createElement('h2', {}, "I am h2 tag")
+        [React.createElement('h1', {id: 'child1', key: 'child1'}, "I am h1 tag"),
+            React.createElement('h2', {id: 'child2', key: 'child2'}, "I am h2 tag")
         ]
     )
 )
@@ -19,5 +12,5 @@ const parent = React.createElement('div', {id: 'parent'},
 
 const heading = React.createElement('h1', {className: "heading"}, "Hello, Milan! good luck mastering react");
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// root.render(heading);
 root.render(parent);
