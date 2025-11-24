f475a950552b9c6446570defcee504a56857230d
milan-2604 <milan2003oli@gmail.com>
2025-11-24 11:58:36 +0530

    ep 3 half done todo react components

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div', {id: 'parent'},
-    React.createElement('div',{id: 'child'},
-        [React.createElement('h1', {id: 'child1', key: 'child1'}, "I am h1 tag"),
-            React.createElement('h2', {id: 'child2', key: 'child2'}, "I am h2 tag")
-        ]
-    )
-)
+// const heading = React.createElement("h1",{id:"heading"},"Namaste React");
 
+//using jsx which is html or xml like syntax
+const jsxHeading = <h1 id="heading">Namaste React</h1>; //normal js engine cant understand this jsx syntax
+//your bundler parcel has Babel which is transpiling this jsx syntax so it can be executed properly
+//jsx=>Babel transpiles this into React.createElement=>ReactElement js object => HTMLElement(render)
 
-const heading = React.createElement('h1', {className: "heading"}, "Hello, Milan! good luck mastering react");
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(parent);
+root.render(jsxHeading);
