de49c13c6ffcfd15739604c21912f932704ca4e9
milan-2604 <milan2003oli@gmail.com>
2025-11-26 11:03:06 +0530

    completed half of ep4 learned about props and how do we use it

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import ReactDOM from "react-dom/client";
  -Search
  -RestrauntContainer
   -RestrauntCard
+   -Img
+   -Name of res, Star Rating, Cuisine, Delivery Time
 -Footer
  -Copyright
  -Links
@@ -22,8 +24,7 @@ const Header = () => {
     <div className="header">
       <img
         className="logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Foodlogo.svg/1027px-Foodlogo.svg.png"
-        width={100}
+        src="https://upload.wikimedia.org/wikipedia/en/6/6a/Ashleylogo.png?20230129125245"
       />
       <div className="nav-items">
         <ul>
@@ -37,12 +38,45 @@ const Header = () => {
   );
 };
 
+//RestrauntCard Component
+const RestrauntCard = (props) => {
+  return (
+    <div className="res-card">
+      <img className="res-image" src={props.resImg} />
+      <h3>{props.name}</h3>
+      <h4 className="rating">{props.rating}</h4>
+      <h4 className="cuisine">{props.cuisine}</h4>
+      <h4 className="del-time">{props.delTime}</h4>
+    </div>
+  );
+};
+
+//Body Component
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* RestrauntCard */}
+        <RestrauntCard
+          name="Megna foods"
+          resImg="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/%22Hyderabadi_Dum_Biryani%22.jpg/500px-%22Hyderabadi_Dum_Biryani%22.jpg"
+          rating="4.5 stars"
+          cuisine="Biryani, North Indian, Asian"
+          delTime="38 minutes"
+        />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
-    <div id="app">
+    <div className="app">
       {/* Header */}
       <Header />
       {/* Body */}
+      <Body />
       {/* Footer */}
     </div>
   );
